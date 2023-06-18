a8c6372d4b398706981370e4d97d2eef61c516ed
kdrzymalski <drzymalski.krzysztof@gmail.com>
2023-06-18 14:49:31 +0200

    getImageUrl function

---
+++ Readme.md
@@ -0,0 +1,15 @@
+## local testing
+
+run
+```sh
+npm install -g netlify-cli
+```
+then
+
+```sh
+netlify dev
+```
+
+## deploy 
+
+Just push to master repo
---
+++ netlify/functions/getImageUrl.js
@@ -0,0 +1,18 @@
+exports.handler = async (event) => {
+    try {
+
+       const exportTask = JSON.parse(event.body).tasks.filter((task) => {
+            return task.name === "export-my-file"
+        })
+
+        return {
+            statusCode: 200,
+            body: exportTask[0].result.files[0].url,
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+};
